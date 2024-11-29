29d1d61ee8bcbbeb58f31e8ef15e757e19e1f717
bakarova05 <124010679+bakarova05@users.noreply.github.com>
2024-11-29 11:20:43 +0700

    Add files via upload
    f

---
+++ test_version.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE test_version
+#include "lib.h"
+#include "lab2.h"
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_version)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+    std::vector<std::vector<int>> arr_test = {{10}, {14}, {15}, {10}, {18}, {20}};
+    BOOST_CHECK(Sort(arr_test[0], arr_test[1]));
+    BOOST_CHECK(Sort(arr_test[2], arr_test[3]));
+    BOOST_CHECK(Sort(arr_test[4], arr_test[5]));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
